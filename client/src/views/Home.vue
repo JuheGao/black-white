babe5275098121b567717c8462c25fb50bdded8f
Juhe <502853139@qq.com>
2020-05-10 18:08:30 +0800

    vux

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home">
+    Home
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <RecordModule></RecordModule>
+    <Actionsheet></Actionsheet>
   </div>
 </template>
 
 <script>
+import { Actionsheet  } from 'vux'
+import RecordModule from '@/components/recordModule'
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    RecordModule,
+    Actionsheet
+  }
 }
 </script>
